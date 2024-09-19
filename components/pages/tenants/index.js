10d80d1100ef88998cf212bbf8b155d871fc041c
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-09-20 02:43:08 +0300

    fix for deploy

--- components/pages/tenants/index.js
+++
@@ -1,3 +0,0 @@
-export { default as AddUpdateModal } from './add-update-modal';
-export { default as LimitPrintView } from './limitPrintView';
-export { default as PrintView } from './printView';
--- components/pages/towers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as AddUpdateModal } from './add-update-modal';
-export { default as LimitPrintView } from './limitPrintView';
-export { default as PrintView } from './printView';
-export { default as Filter } from './filter';
-export { default as ApartmentsDetails } from './apartments-details';
