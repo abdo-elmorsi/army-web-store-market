160a04b9ded25d385872f810debb2b4afbee6d1e
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-10-12 02:08:32 +0300

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -46,24 +46,24 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#336a86" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#336a86" />
-          <meta name="msapplication-config" content="/public/browserconfig.xml" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
 
           {/* Icons */}
-          <link rel="apple-touch-icon" sizes="152x152" href="/public/touch-icon/touch-icon-ipad.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/public/touch-icon/touch-icon-iphone-retina.png" />
-          <link rel="apple-touch-icon" sizes="167x167" href="/public/touch-icon/touch-icon-ipad-retina.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png" />
-          <link rel="shortcut icon" href="/public/favicon.ico" />
-          <link rel="mask-icon" href="/public/safari-pinned-tab.svg" color="#336a86" />
-          <link rel="manifest" href="/public/manifest.json" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/touch-icon/touch-icon-ipad.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/touch-icon/touch-icon-iphone-retina.png" />
+          <link rel="apple-touch-icon" sizes="167x167" href="/touch-icon/touch-icon-ipad-retina.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#336a86" />
+          <link rel="manifest" href="/manifest.json" />
 
           {/* Social Media Meta Tags */}
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://army-web-store-market.vercel.app" />
           <meta name="twitter:title" content="Army | Store Market" />
           <meta name="twitter:description" content="Best Army Store Market App in the world" />
-          <meta name="twitter:image" content="/public/icon-192x192.png" />
+          <meta name="twitter:image" content="/icon-192x192.png" />
           <meta name="twitter:creator" content="@Abdo_Elmorsi" />
 
           <meta property="og:type" content="website" />
@@ -71,7 +71,7 @@ class MyDocument extends Document {
           <meta property="og:description" content="Best Army Store Market App in the world" />
           <meta property="og:site_name" content="Army | Store Market" />
           <meta property="og:url" content="https://army-web-store-market.vercel.app" />
-          <meta property="og:image" content="/public/apple-touch-icon.png" />
+          <meta property="og:image" content="/apple-touch-icon.png" />
 
           {/* Language Support */}
           <meta httpEquiv="content-language" content="ar, en" />
