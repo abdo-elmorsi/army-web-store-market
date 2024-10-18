96f7476fc8c4179cbde4b8df728c5deb72ed24c6
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-10-18 12:38:18 +0300

    disable action

---
+++ pages/api/disable-ation/index.js
@@ -0,0 +1,17 @@
+import { handleDeleteRequest, handleGetRequest, handlePostRequest, handlePutRequest } from "lib/controllers/users-controller";
+
+const handler = async (req, res) => {
+	const { method } = req;
+	try {
+		switch (method) {
+			case "GET":
+				await res.status(201).json(true)
+				break;
+			default:
+				res.status(405).json({ message: "Method Not Allowed" });
+		}
+	} catch (error) {
+		res.status(500).json({ message: error.message || "An internal server error occurred." });
+	}
+};
+export default handler
